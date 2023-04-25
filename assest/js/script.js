427b2dc69ee51062f9264fb7f827274c315da883
Akash Khan <akashcc48@gmail.com>
2023-04-25 09:04:31 +0600

    this is my portfolio

---
+++ assest/js/script.js
@@ -0,0 +1,10 @@
+/* =================Typing Animation============ */
+
+var typed = new Typed('.typing', {
+    strings: ['', 'Web Designer', 'Web Developer', 'Facebook Marketer', 'React Js Developer', 'Redux Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    startDelay: 1000,
+    backDelay: 1500,
+    loop: true,
+})
